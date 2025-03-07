9ef3ccc536e8a7fd515ac01f0b92a51c0ef18f75
cleialopes <cleialopes530@gmail.com>
2025-03-07 10:26:56 +0100

    add

--- database.js
+++ database.js
@@ -27,27 +27,27 @@ db.serialize(() => {
   )
   `);
 
-  db.get('SELECT * FROM users WHERE email = ?', ['cleialopes530@gmail.com'], (err, row) => {
-    if (err) {
-      console.error("Error verificando el usuario admin:", err);
-      return;
-    }
+  // db.get('SELECT * FROM users WHERE email = ?', ['cleialopes530@gmail.com'], (err, row) => {
+  //   if (err) {
+  //     console.error("Error verificando el usuario admin:", err);
+  //     return;
+  //   }
     
-    if (row) {
-      console.log("✅ Usuario administrador ya existe en la base de datos.");
-    } else {
-      console.log("⚠️ Usuario administrador no encontrado. Creando nuevo usuario...");
-      db.run(`
-        INSERT INTO users (name, username, telefono, email, password, isadmin)
-        VALUES (?, ?, ?, ?, ?, ?)`,
-        ['jozicleia', 'cleia', '0000000000', 'cleialopes530@gmail.com', '$2b$10$Q.WjvVp3K/fNpklJ7KY6TuLJH7F3G6N4hT1j.oHQGSAwIH7v3MX5K', 1],
-        (err) => {
-          if (err) console.error("Error insertando usuario admin:", err);
-          else console.log("✅ Usuario administrador creado exitosamente.");
-        }
-      );
-    }
-  });
+  //   if (row) {
+  //     console.log("✅ Usuario administrador ya existe en la base de datos.");
+  //   } else {
+  //     console.log("⚠️ Usuario administrador no encontrado. Creando nuevo usuario...");
+  //     db.run(`
+  //       INSERT INTO users (name, username, telefono, email, password, isadmin)
+  //       VALUES (?, ?, ?, ?, ?, ?)`,
+  //       ['jozicleia', 'cleia', '0000000000', 'cleialopes530@gmail.com', '$2b$10$Q.WjvVp3K/fNpklJ7KY6TuLJH7F3G6N4hT1j.oHQGSAwIH7v3MX5K', 1],
+  //       (err) => {
+  //         if (err) console.error("Error insertando usuario admin:", err);
+  //         else console.log("✅ Usuario administrador creado exitosamente.");
+  //       }
+  //     );
+  //   }
+  // });
 });
 
 module.exports = db;
